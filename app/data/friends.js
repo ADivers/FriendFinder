ad8a1a18f987963a516f13f91c2f68289b0fb452
Divers <divers1776@gmail.com>
2017-08-14 20:05:09 -0400

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Reagan',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/Ronald_Reagan_with_cowboy_hat_12-0071M_edit.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Richard',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Richard_M._Nixon%2C_ca._1935_-_1982_-_NARA_-_530679.jpg/220px-Richard_M._Nixon%2C_ca._1935_-_1982_-_NARA_-_530679.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Carter',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/DIG13605-028.jpg/200px-DIG13605-028.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Bill',
+		photo: 'http://c2.nrostatic.com/sites/default/files/styles/original_image_with_cropping/public/uploaded/bill-clinton-sex-scandals-hillary-campaign.jpg?itok=W6yOiBz2',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'George',
+		photo: 'https://boardgamegeek.com/camo/d09516b1b4e30622d5d7b8a539758e7de8fa7ad1/687474703a2f2f7777772e6e6577796f726b65722e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031362f31302f426f726f7769747a2d5472756d702d457870616e64732d41747461636b2d47656f7267652d436c696e746f6e2d313230302e6a7067',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Barrack',
+		photo: 'http://cdn.inquisitr.com/wp-content/uploads/2016/08/president-barack-obama.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Bernie',
+		photo: 'https://pbs.twimg.com/profile_images/794596124686487552/kqpbolIc.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Hills',
+		photo: 'https://thenypost.files.wordpress.com/2015/10/usa-election_clinton2.jpg?quality=90&strip=all&w=664&h=441&crop=1',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Magic',
+		photo: 'https://pbs.twimg.com/profile_images/735621697642975232/YznfoCzt.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Ed',
+		photo: 'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i88/eee_180x180.png',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
